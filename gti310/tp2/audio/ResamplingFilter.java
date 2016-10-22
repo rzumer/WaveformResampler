652e759c10e514359dc209296575cd01cee53774
Raphael Zumer <rzumer@gmail.com>
2016-10-22 17:27:51 -0400

    Add MathExtensions class to calculate resampling data
    Former-commit-id: 36ae978d4f5f8784fb85e26d9dd6760e668d7076

--- gti310/tp2/audio/ResamplingFilter.java
+++ gti310/tp2/audio/ResamplingFilter.java
@@ -5,6 +5,31 @@ public class ResamplingFilter extends AudioFilter
 	@Override
 	public byte[] process(byte[] input)
 	{
+		return process(input, 8000); // Default output sample rate of 8000 Hz
+	}
+	
+	public byte[] process(byte[] input, int sampleRate)
+	{
 		return input;
+	}
+}
+
+final class MathExtensions
+{
+	public static int LeastCommonMultiple(int a, int b)
+	{
+		return a / GreatestCommonDivisor(a, b) * b;
+	}
+	
+	public static int GreatestCommonDivisor(int a, int b)
+	{
+		while(b != 0)
+		{
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		
+		return a;
 	}
 }
