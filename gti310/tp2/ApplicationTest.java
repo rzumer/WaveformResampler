7d8931be60334c135fef469059103d76d99f832e
Raphael Zumer <rzumer@gmail.com>
2016-10-22 17:31:06 -0400

    Add JUnit tests Unit test for math extensions and integration test for main application
    Former-commit-id: 90ccc2590f0514fed7dc89b2bfcf5e40ed172428

---
+++ gti310/tp2/ApplicationTest.java
@@ -0,0 +1,52 @@
+package gti310.tp2;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import gti310.tp2.audio.AudioController;
+import gti310.tp2.audio.HeaderFormatException;
+import gti310.tp2.audio.ResamplingFilter;
+import gti310.tp2.audio.UnsupportedFormatException;
+import gti310.tp2.audio.WaveController;
+import gti310.tp2.io.FileSink;
+import gti310.tp2.io.FileSource;
+
+public class ApplicationTest {	
+	@Test
+	public void test()
+	{
+		try
+		{
+			final String InputFileName = "media/App1Test1Stereo16bits.wav";
+			final String OutputFileName = "test_out.wav";
+			
+			FileSource input = new FileSource(InputFileName);
+			FileSink output = new FileSink(OutputFileName);
+			
+			AudioController controller = new WaveController(input, output);
+			controller.applyFilter(new ResamplingFilter());
+			controller.close();
+			
+			File outputFile = new File(OutputFileName);
+			outputFile.deleteOnExit();
+			
+			if(!outputFile.exists() || outputFile.length() <= 0)
+			{
+				fail("Output File Not Found");
+			}
+			
+		} catch (FileNotFoundException e) {
+			fail("I/O Error");
+		} catch (HeaderFormatException e) {
+			fail("Invalid Input");
+		} catch (UnsupportedFormatException e) {
+			fail("Unsupported Input");
+		} catch (Exception e) {
+			fail("Unknown Exception");
+		}
+	}
+}
---
+++ gti310/tp2/audio/MathTest.java
@@ -0,0 +1,21 @@
+package gti310.tp2.audio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathTest {
+
+	@Test
+	public void test() {
+		int lcm = MathExtensions.LeastCommonMultiple(44100, 8000);
+		assertEquals(lcm, 3528000);
+		
+		lcm = MathExtensions.LeastCommonMultiple(44000, 8000);
+		assertEquals(lcm, 88000);
+		
+		lcm = MathExtensions.LeastCommonMultiple(48000, 44000);
+		assertEquals(lcm, 528000);
+	}
+
+}
