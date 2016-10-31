903a3d4001025e8b624164333e4a2af99042d53a
Raphael Zumer <rzumer@gmail.com>
2016-10-31 11:24:25 -0400

    Add detailed comments to FastResamplingFilter

--- audioresampler/audio/FastResamplingFilter.java
+++ audioresampler/audio/FastResamplingFilter.java
@@ -66,34 +66,50 @@ public class FastResamplingFilter extends ResamplingFilter
 		// Iteration
 		int frameCount = 0;
 
-		// O(n) - Iterate through frames, dependent on n.
+		// O(n) - Iterate through frames, dependent on n (input length).
 		for(;;)
 		{
+			// The next location of the sample to retrieve. If the value is decimal, it is between two frames.
 			double framePointer = MathHelper.Round(segmentOffset + (frameCount * decimationRate), DecimalPlaces);
+			// The decimal portion of the frame pointer.
 			double framePointerDecimal = MathHelper.Round(framePointer % 1, DecimalPlaces);
+			/* An adjusted, positive value of the frame pointer's decimal portion,
+			 * representing the distance between the left frame and the right frame to interpolate.
+			 * When downsampling, it is the same value as framePointerDecimal. When upsampling,
+			 * consecutive input data will result in a negative offset, meaning the last frame
+			 * processed in the last set of input data is interpolated with the first frame of the
+			 * current input data to obtain the correct number of samples in output. In this case,
+			 * framePointerDecimal will be a negative value, and must be adjusted for interpolation.
+			 */
 			double weight = MathHelper.Round((framePointerDecimal + 1) % 1, DecimalPlaces);
 
+			// The frame numbers used to perform interpolation.
 			int leftFrameNumber = (int) Math.round(framePointer - weight);
 			int rightFrameNumber = leftFrameNumber + 1;
 
-			// The while check is insufficient to ensure that the new left frame
-			// and the right frame, if one is needed, are present in this segment.
+			// Ensure that the input data is large enough to contain the frames required in this loop.
+			// If the weight is 0, only one frame is needed, the left frame. Otherwise, two frames are needed.
+			// When upsampling, one last loop will be performed with empty input data to add extra frames based
+			// on the remaining offset. The second test ensures that there is no missing or extra sample(s) added.
 			if(input.length < (framePointerDecimal > 0 ? rightFrameNumber : leftFrameNumber) * frameSize + frameSize || (input.length == 0 && (int)Math.round(framePointerDecimal) >= 0))
 			{
 				break;
 			}
 
 			// Read samples in the left and right frames.
-			// Left samples are null on the first frame when upsampling.
+			// Left samples are null on the first frame when upsampling, due to a negative offset.
+			// Right samples are null when the weight is 0, meaning an input sample is simply copied to the output.
 			int[] leftSamples = leftFrameNumber < 0 ? readFrameSamples(lastFrameProcessed) : readFrameSamples(inputBuffer, leftFrameNumber);
 			int[] rightSamples = weight > 0 ? readFrameSamples(inputBuffer, rightFrameNumber) : null;
 
-			// Perform linear interpolation for each sample.
 			int[] interpolatedSamples = new int[properties.NumChannels];
 
 			// O(1) - Iterate through channels, independent from n.
 			for(int channel = 0; channel < properties.NumChannels; channel++)
 			{
+				// Perform the actual interpolation.
+				// When the left sample is set to 0 (because the bytes are null), the right sample will be interpolated with 0.
+				// When the right sample is set to 0, then the weight is typically equal to 0, and the left sample will be selected as is.
 				interpolatedSamples[channel] = MathHelper.InterpolateLinear(leftSamples == null ? 0 : leftSamples[channel], rightSamples == null ? 0 : rightSamples[channel], weight);
 
 				try
@@ -123,17 +139,15 @@ public class FastResamplingFilter extends ResamplingFilter
 				}
 			}
 
-			// Basic depth checking.
-			if(frameCount++ > Math.round(input.length / (decimationRate * frameSize) + segmentOffset + frameSize))
-			{
-				throw new IndexOutOfBoundsException();
-			}
+			// Increment the output frame count.
+			frameCount++;
 
 			// Place the buffer back to the first byte of the left frame.
 			inputBuffer.position(Math.max(leftFrameNumber * frameSize, 0));
 		}
 
-		// Return
+		// Set the last frame processed and the segment offset in order to process the next data segment properly.
+		// This is skipped on the last empty segment, as there is no actual frame being processed.
 		if(input.length >= frameSize)
 		{
 			lastFrameProcessed = Arrays.copyOfRange(input, input.length - frameSize, input.length);
